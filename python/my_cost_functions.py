4ca849085649233f320915f1c9430d08fd7eff93
Ramiz Raja <informramiz@gmail.com>
2017-08-08 04:25:04 +0500

    feat: Define namedtuple TrajectoryData

--- python/my_cost_functions.py
+++ python/my_cost_functions.py
@@ -1,3 +1,19 @@
+from __future__ import division
+from collections import namedtuple
+from math import sqrt,exp
+import pdb
+
+TrajectoryData = namedtuple("TrajectoryData", [
+        "proposed_lane",
+        "avg_speed",
+        "max_acceleration",
+        "rms_acceleration",
+        "closest_approach",
+        "end_distance_to_goal",
+        "end_lanes_from_goal",
+        "collides",
+        ])
+
 def calculate_cost(vehicle, trajectory, predictions):
     #TODO: implement
-    return None
+    return 0
