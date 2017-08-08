5cd62d1962697375bbd43ed4d41086021cd91825
Ramiz Raja <informramiz@gmail.com>
2017-08-08 16:41:38 +0500

    feat: Add function to filter predicted trajectories by lane

--- python/my_cost_functions.py
+++ python/my_cost_functions.py
@@ -17,3 +17,15 @@ TrajectoryData = namedtuple("TrajectoryData", [
 def calculate_cost(vehicle, trajectory, predictions):
     #TODO: implement
     return 0
+"""
+Filter predicted trajectories whose first prediction is in in given lane
+"""
+def filter_predictions_by_lane(predictions, lane):
+    filtered = {}
+    for v_id, trajectory in predictions.items():
+        #-1 is vehicle_id for ego vehicle so we don't want to consider
+        #ego vehicle predictions if any
+        if trajectory[0]['lane'] == lane and v_id != -1:
+            filtered[v_id] = trajectory;
+
+    return filtered
