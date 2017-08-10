532c5891a186a87ee18a3bade0c230cec2dddfed
Ramiz Raja <informramiz@gmail.com>
2017-08-11 02:38:46 +0500

    feat: Add constructor

--- src/trajectory_data.h
+++ src/trajectory_data.h
@@ -32,6 +32,27 @@ struct TrajectoryData {
   bool is_collision_detected;
   //variable to hold timestep at which collision happens, if any
   int collides_at;
+
+  TrajectoryData(int proposed_lane,
+                 double avg_speed,
+                 double max_acceleration,
+                 double mean_squared_acceleration,
+                 double closes_approach,
+                 double end_distance_to_goal,
+                 double end_distance_from_goal_lane,
+                 double is_collision_detected,
+                 int collides_at) {
+
+    this->proposed_lane = proposed_lane;
+    this->avg_speed = avg_speed;
+    this->max_acceleration = max_acceleration;
+    this->mean_squared_acceleration = mean_squared_acceleration;
+    this->closest_approach = closes_approach;
+    this->end_distance_to_goal = end_distance_to_goal;
+    this->end_distance_from_goal_lane = end_distance_from_goal_lane;
+    this->is_collision_detected = is_collision_detected;
+    this->collides_at = collides_at;
+  }
 };
 
 
